fbc966a8238ac23c1123691d019661768f339bf0
kasambuli <cynthiakasambuli@gmail.com>
2018-09-26 17:09:58 +0300

    add folder structure

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return ( <
+            div className = "App" >
+            <
+            header className = "App-header" >
+            <
+            h1 className = "App-title" > Welcome to Movie DB < /h1> <
+            /header> <
+            p className = "App-intro" >
+            This is a website that shows a user the current, popular and upcoming movies. <
+            /p> <
+            /div>
+        );
+    }
+}
+
+export default App;
