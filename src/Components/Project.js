e6ceab635d12710d2f1f03f2b4a954ed92221d84
kasambuli <cynthiakasambuli@gmail.com>
2018-10-01 16:30:49 +0300

    added Project.js file

---
+++ src/Components/Project.js
@@ -0,0 +1,61 @@
+import React, {
+    Component
+} from 'react';
+
+class Project extends Component {
+    constructor() {
+        super();
+        this.state = {
+            overviews: [],
+        }
+    };
+
+    // imagePath(poster_path) {
+    //     const path = "https: //image.tmdb.org/t/p/w500/" + poster_path
+    //     return path
+    // }
+
+    componentDidMount() {
+        fetch('https://api.themoviedb.org/3/movie/popular?api_key=f0d5eb420321dd173b56e25b8d03f7f2').then(response => {
+            return response.json();
+
+        }).then(data => {
+            console.log(data)
+            let overviews = data.results
+            this.setState({ overviews: overviews });
+            // console.log("state", this.state.overviews);
+        })
+    }
+    posterPath(path) {
+        const poster = "https://image.tmdb.org/t/p/w500/";
+        return poster + path
+    }
+    render() {
+        var { overviews } = this.state
+
+
+
+        return ( < div className = "Project" > {
+                overviews.map((view) => ( <
+                    div className = "ProjectItem"
+                    key = { view.id } > < br / >
+                    <
+                    a href = '#' > <
+                    img src = { this.posterPath(view.poster_path) }
+                    alt = { view.title }
+                    /> </a > < br / > < p > { view.title } < /p>< /div >
+                ))
+            } < /div>)
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+    export default Project;
