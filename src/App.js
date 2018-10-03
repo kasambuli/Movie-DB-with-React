b0da0f62edf824d0768ca8267b544a5ecc632177
kasambuli <cynthiakasambuli@gmail.com>
2018-10-03 15:52:12 +0300

    restructured file after adding prettifier on my text editor

--- src/App.js
+++ src/App.js
@@ -5,20 +5,13 @@ import Project from './Components/Project';
 
 class App extends Component {
     render() {
-        return ( <
-            div className = "App" >
-            <
-            header className = "App-header" >
-            <
-            h1 className = "App-title" > Welcome to Movie DB < /h1> < /
-            header > <
-            p className = "App-intro" >
-            This is a website that shows a user the current, popular and upcoming movies. <
-            /p>   <
-            Project / >
-            <
-            /
-            div >
+        return ( <div className = "App">
+            <header className ="App-header">
+            <h1 className = "App-title"> Welcome to Movie DB </h1></header> <p className = "App-intro">
+            This is a website that shows a user the current, popular and upcoming movies. </p>
+            <p>alilili</p>   
+            <Project/>
+            </div>
         );
     }
 }
